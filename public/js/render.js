c6de30e28cc74008237ee0fefeb0521f23b2fcfe
royvanderzon <royvanderzon@gmail.com>
2016-11-01 21:54:15 +0100

    Render fix

--- public/js/render.js
+++ public/js/render.js
@@ -28,66 +28,66 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("data.csv", function(error, data) {
-  if (error) throw error;
+// d3.csv("data.csv", function(error, data) {
+//   if (error) throw error;
 
-  var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
+//   var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
 
-  data.forEach(function(d) {
-    d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
-  });
+//   data.forEach(function(d) {
+//     d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+//   });
 
-  x0.domain(data.map(function(d) { return d.State; }));
-  x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
-  y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
+//   x0.domain(data.map(function(d) { return d.State; }));
+//   x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
+//   y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+//   svg.append("g")
+//       .attr("class", "x axis")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Population");
+//   svg.append("g")
+//       .attr("class", "y axis")
+//       .call(yAxis)
+//     .append("text")
+//       .attr("transform", "rotate(-90)")
+//       .attr("y", 6)
+//       .attr("dy", ".71em")
+//       .style("text-anchor", "end")
+//       .text("Population");
 
-  var state = svg.selectAll(".state")
-      .data(data)
-    .enter().append("g")
-      .attr("class", "state")
-      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
+//   var state = svg.selectAll(".state")
+//       .data(data)
+//     .enter().append("g")
+//       .attr("class", "state")
+//       .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
 
-  state.selectAll("rect")
-      .data(function(d) { return d.ages; })
-    .enter().append("rect")
-      .attr("width", x1.rangeBand())
-      .attr("x", function(d) { return x1(d.name); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .style("fill", function(d) { return color(d.name); });
+//   state.selectAll("rect")
+//       .data(function(d) { return d.ages; })
+//     .enter().append("rect")
+//       .attr("width", x1.rangeBand())
+//       .attr("x", function(d) { return x1(d.name); })
+//       .attr("y", function(d) { return y(d.value); })
+//       .attr("height", function(d) { return height - y(d.value); })
+//       .style("fill", function(d) { return color(d.name); });
 
-  var legend = svg.selectAll(".legend")
-      .data(ageNames.slice().reverse())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+//   var legend = svg.selectAll(".legend")
+//       .data(ageNames.slice().reverse())
+//     .enter().append("g")
+//       .attr("class", "legend")
+//       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
+//   legend.append("rect")
+//       .attr("x", width - 18)
+//       .attr("width", 18)
+//       .attr("height", 18)
+//       .style("fill", color);
 
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
+//   legend.append("text")
+//       .attr("x", width - 24)
+//       .attr("y", 9)
+//       .attr("dy", ".35em")
+//       .style("text-anchor", "end")
+//       .text(function(d) { return d; });
 
-});
+// });
