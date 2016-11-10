ae9d064ce893f1f88d4720c055398a948515147e
Niels de Bruin <niels@nielsdB97.nl>
2016-11-10 14:43:45 +0100

    Bit of jQuery to search within all modals for an element which has the autofocus attribute and focus on it

--- public/js/handle.js
+++ public/js/handle.js
@@ -233,11 +233,16 @@ $(document).ready(function(){
 
 	$('.saveState').click(function(){
 		$('#saveStateName').modal();
-	})
+	});
 
 	$('.closeSaveModal').click(function(){
 		$('#notLongError').slideUp();
-	})
+	});
+
+  // Every time a modal is shown, if it has an autofocus element, focus on it.
+  $('.modal').on('shown.bs.modal', function() {
+    $(this).find('[autofocus]').focus();
+  });
 
 	$('#saveStateNameButton').click(function(){
 
